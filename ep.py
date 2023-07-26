ce238a4ee97492f3bf4718b9515300578d49854d
anajuliassc <105124468+anajuliassc@users.noreply.github.com>
2023-07-26 00:48:07 -0300

    add new line code

--- ep.py
+++ ep.py
@@ -6,12 +6,20 @@ cam = cv.VideoCapture(0)
 while True:
     ret, frame = cam.read()
 
-    img_hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV) 
-    verde_lower = np.array([])
-    verde_upper = np.array([])
-    
-    mask = cv.inRange(img_hsv, )
+    img_hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+    verde_lower = np.array([20, 50, 70])
+    verde_upper = np.array([100, 255, 255])
 
+    mask = cv.inRange(img_hsv, verde_lower, verde_upper)
+    contours, hierarchy = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+
+    if len(contours) != 0:
+        for contour in contours:
+            if cv.contourArea(contour) > 500:
+                g,  r, e, n = cv.boundingRect(contour)
+                cv.rectangle(frame, (g,r), (g + e, r + n), (0, 255, 0), 3)
+
+   
     cv.imshow('frame', frame)
 
     if cv.waitKey(1) & 0xFF == ord('q'):
